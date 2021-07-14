fc009b9624cfab51f00da96b251a535aad8cdf09
Ricard Garcia <imricardgarcia@gmail.com>
2021-07-14 10:18:58 +0200

    feat: added main view

--- views/main/main.php
+++ views/main/main.php
@@ -1,1 +1,4 @@
 <!-- This is the main generic view of your application, it's not required to use it -->
+<h1 class='main-title'>Main view</h1>
+<a href='./index.php?controller=employees&action=getAllEmployees' class='btn btn-outline-primary'>Employees</a>
+<a href='./index.php?controller=departments&action=getAllDepartments' class='btn btn-outline-secondary'>Departments</a>
