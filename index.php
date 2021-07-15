beb8fc436c28b7de2d5682aec8f62954fd99429d
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-07-15 15:43:56 +0200

    Deleted todo

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 
 include_once "config/constants.php";
 
-// TODO Implement the logic to include the controller passed by the URL dynamically
 // In the event that the controller passed by URL does not exist, you must show the error view.
 
 if (isset($_GET['controller'])) {
